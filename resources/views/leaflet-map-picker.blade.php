3789aa12c91bf28471dabe6c638d7836eecbe37c
Azad Furkan ŞAKAR <afsakarr@gmail.com>
2025-04-23 10:29:18 +0300

    Fix search results list height

--- resources/views/leaflet-map-picker.blade.php
+++ resources/views/leaflet-map-picker.blade.php
@@ -47,7 +47,7 @@
             <x-slot name="heading">
                 {{ __('filament-leaflet-map-picker::leaflet-map-picker.search_location') }}
             </x-slot>
-            
+
             <div class="space-y-4">
                 <div class="relative">
                     <x-filament::input.wrapper  suffix-icon="heroicon-m-magnifying-glass">
@@ -59,19 +59,19 @@
                         />
                     </x-filament::input.wrapper>
                 </div>
-                
+
                 <!-- Loading indicator -->
                 <div x-show="isSearching" class="flex justify-center py-4">
                     <x-filament::loading-indicator class="h-5 w-5" />
                 </div>
-                
+
                 <!-- Search results -->
-                <div 
-                    x-show="localSearchResults.length > 0 && !isSearching"                 
+                <div
+                    x-show="localSearchResults.length > 0 && !isSearching"
                     x-cloak
                     class="bg-white dark:bg-gray-700 rounded-lg overflow-hidden border border-gray-200 dark:border-gray-600 shadow-sm"
                 >
-                    <ul class="max-h-60 overflow-auto">
+                    <ul class="overflow-auto">
                         <template x-for="(result, index) in localSearchResults" :key="index">
                             <li class="hover:bg-gray-50 dark:hover:bg-gray-800 transition-colors duration-150">
                                 <button
@@ -93,10 +93,10 @@
                         </template>
                     </ul>
                 </div>
-                
+
                 <!-- No results message -->
-                <div 
-                    x-show="searchQuery && searchQuery.length > 2 && localSearchResults.length === 0 && !isSearching" 
+                <div
+                    x-show="searchQuery && searchQuery.length > 2 && localSearchResults.length === 0 && !isSearching"
                     class="bg-gray-50 dark:bg-gray-700 rounded-lg p-4 text-center"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto text-gray-400 dark:text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -107,7 +107,7 @@
                     </p>
                 </div>
             </div>
-            
+
             <x-slot name="footer">
                 <x-filament::button color="gray" @click="$dispatch('close-modal', { id: 'location-search-modal' })">
                     {{ __('filament-leaflet-map-picker::leaflet-map-picker.cancel') }}
