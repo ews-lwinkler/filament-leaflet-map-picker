59ef43907098491487783e5af1ac8050869d3241
Lukas Winkler <l.winkler@ews-consulting.com>
2025-08-14 07:29:30 +0200

    filament 4 updates

---
+++ resources/lang/de/leaflet-map-picker.php
@@ -0,0 +1,11 @@
+<?php
+
+// translations for Afsakar/LeafletMapPicker
+return [
+    'selected_locations' => 'Ausgewählter Standort:',
+    'map_type' => 'Kartentyp',
+    'search_location' => 'Standort suchen',
+    'search_placeholder' => 'Standort suchen...',
+    'no_results' => 'Keine Ergebnisse gefunden',
+    'cancel' => 'Abbrechen',
+];
